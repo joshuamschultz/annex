9f67baa75f74de7f7895dd1006f10e08997b11cd
Joshua Schultz <joshuamschultz@gmail.com>
2019-09-12 12:45:17 -0400

    migrations

--- db/migrate/20190912163008_add_items_to_import_part.rb
+++ db/migrate/20190912163008_add_items_to_import_part.rb
@@ -1,7 +1,7 @@
 class AddItemsToImportPart < ActiveRecord::Migration[6.0]
   def change
-    add_column :import_parts, :cost, :decimal
-    add_column :import_parts, :country_of_origin, :string
-    add_column :import_parts, :quantity, :integer
+    add_column :imports_parts, :cost, :decimal
+    add_column :imports_parts, :country_of_origin, :string
+    add_column :imports_parts, :quantity, :integer
   end
 end
