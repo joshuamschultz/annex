7413c7e3e8fe324dfd4274d495cebc5a4b3f40ef
guinex <sachavha@syr.edu>
2019-10-22 10:29:33 -0400

    partial changes datatable
    uncommenting line 24 gives error

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // $('#usage_block').dataTable({
+  //   "processing": true,
+  //   "serverSide": true,
+  //   "ajax": {
+  //     "url": $('#usage_block').dataset['source']
+  //   },
+  //   "pagingType": "full_numbers",
+  //   "columns": [
+  //   ]
+  // });
+});
