da382fa502f36cc7ebaa9d59c1480828cd97a879
Joshua Schultz <joshuamschultz@gmail.com>
2019-11-15 14:33:06 -0500

    usage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,13 +19,13 @@ require("jquery")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap'
-import "../stylesheets/application"
+import 'bootstrap';
+import "../stylesheets/application";
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
 
-require('datatables.net-dt')(window, $)
+
 import "cocoon-js"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,8 @@
 const { environment } = require('@rails/webpacker')
-
 const webpack = require('webpack')
-
-
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
-
 module.exports = environment
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
